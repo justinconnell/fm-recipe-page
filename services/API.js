a4461dbda6cead62acede0e5bd5002c6d0db20c7
Justin Connell <137558716+justinconnell@users.noreply.github.com>
2024-02-24 16:21:01 +0200

    fix: gh pages link

--- services/API.js
+++ services/API.js
@@ -1,5 +1,5 @@
 const API = {
-  url: "/data/recipe.json",
+  url: "https://justinconnell.github.io/fm-recipe-page/data/recipe.json",
   fetchRecipe: async () => {
     const result = await fetch(API.url);
     return await result.json();
